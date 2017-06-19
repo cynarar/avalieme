016ba93bb4d7cab1ea82438d25e7d7934d01617e
cynarar <cynara.peixoto@gmail.com>
2017-06-19 20:10:56 +0000

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170616162607) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string   "email",              default: "", null: false
     t.string   "encrypted_password", default: "", null: false
@@ -28,49 +31,12 @@ ActiveRecord::Schema.define(version: 20170616162607) do
     t.datetime "updated_at",                      null: false
   end
 
-  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
-  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["unlock_token"], name: "index_admins_on_unlock_token", unique: true, using: :btree
 
   create_table "cities", force: :cascade do |t|
     t.string   "nome",       null: false
     t.string   "uf",         null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "clientes", force: :cascade do |t|
-    t.string   "nome"
-    t.string   "email"
-    t.string   "telefone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "orcamentos", force: :cascade do |t|
-    t.integer  "clientes_id"
-    t.integer  "servicos_id"
-    t.integer  "tipo_eventos_id"
-    t.datetime "data"
-    t.boolean  "status"
-    t.decimal  "total",           precision: 8, scale: 2
-    t.datetime "created_at",                              null: false
-    t.datetime "updated_at",                              null: false
-  end
-
-  add_index "orcamentos", ["clientes_id"], name: "index_orcamentos_on_clientes_id"
-  add_index "orcamentos", ["servicos_id"], name: "index_orcamentos_on_servicos_id"
-  add_index "orcamentos", ["tipo_eventos_id"], name: "index_orcamentos_on_tipo_eventos_id"
-
-  create_table "servicos", force: :cascade do |t|
-    t.string   "nome"
-    t.decimal  "preco",      precision: 8, scale: 2
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.decimal  "decimal"
-  end
-
-  create_table "tipo_eventos", force: :cascade do |t|
-    t.string   "nome"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -93,7 +59,7 @@ ActiveRecord::Schema.define(version: 20170616162607) do
     t.integer  "city_id"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
